7b8258d0f701e341e208b125cddfc2229fdff472
Dan Gilk <djgilk@gmail.com>
2013-12-05 15:28:59 -0800

    finish basic todo

--- src/com/dan/simpletodo/TodoActivity.java
+++ src/com/dan/simpletodo/TodoActivity.java
@@ -1,15 +1,35 @@
 package com.dan.simpletodo;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class TodoActivity extends Activity {
+	ArrayList<String> items;
+	ArrayAdapter<String> itemsAdapter;
+	ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
     }
 
 
@@ -20,4 +40,43 @@ public class TodoActivity extends Activity {
         return true;
     }
     
+    public void addTodoItem(View v){
+    	EditText etNewItem = (EditText)findViewById(R.id.etNewItem);
+    	itemsAdapter.add(etNewItem.getText().toString());
+    	etNewItem.setText("");
+    	saveItems();
+    }
+    
+    private void setupListViewListener(){
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener(){
+    		@Override
+    		public boolean onItemLongClick(AdapterView<?> aView,View item, int pos, long id){
+    			items.remove(pos);
+    			itemsAdapter.notifyDataSetInvalidated();
+    			saveItems();
+    			return true;
+    		}
+    	});
+    }
+    
+    private void readItems(){
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		items = new ArrayList<String>(FileUtils.readLines(todoFile));
+    	}catch(IOException e){
+    		items = new ArrayList<String>();
+    		e.printStackTrace();
+    	}
+    }
+    
+    private void saveItems(){
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try{
+    		FileUtils.writeLines(todoFile, items);
+    	}catch(IOException e){
+    		e.printStackTrace();
+    	}
+    }
 }
